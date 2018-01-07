a61be05faeb1318c601399c351fbb22283351b62
oitsjustjose <stovallj1995@gmail.com>
2018-01-07 08:58:56 -0500

    Fix up the login screen some

--- Mobile/Account/my-app.js
+++ Mobile/Account/my-app.js
@@ -35,80 +35,77 @@ $(document).ready(function () {
     });
 });
 
-(function createUser() {
-    document.getElementById("new_register").addEventListener('click', () => {
+function register() {
+    var first = document.getElementById("new_first");
+    var last = document.getElementById("new_last");
+    var email = document.getElementById("new_email");
+    var pass = document.getElementById("new_pass");
+    var pass_conf = document.getElementById("new_pass_conf");
+    var good = true;
 
-        var first = document.getElementById("new_first");
-        var last = document.getElementById("new_last");
-        var email = document.getElementById("new_email");
-        var pass = document.getElementById("new_pass");
-        var pass_conf = document.getElementById("new_pass_conf");
-        var good = true;
+    if (first.value === "") {
+        first.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        good = false;
+    }
+    else {
+        first.removeAttribute("style")
+    }
+    if (last.value === "") {
+        last.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        good = false;
+    }
+    else {
+        last.removeAttribute("style")
+    }
+    if (email.value === "") {
+        email.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        good = false;
+    }
+    else {
+        email.removeAttribute("style")
+    }
+    if (pass.value.length < 6) {
+        pass.value = "";
+        pass.setAttribute("placeholder", "Password must be at least 6 chars");
+        pass.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        good = false;
+    }
+    if (pass_conf.value.length < 6) {
+        pass_conf.value = "";
+        pass_conf.setAttribute("placeholder", "Password must be at least 6 chars");
+        pass_conf.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        good = false;
+    }
 
-        if (first.value === "") {
-            first.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
-            good = false;
-        }
-        else {
-            first.removeAttribute("style")
-        }
-        if (last.value === "") {
-            last.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
-            good = false;
-        }
-        else {
-            last.removeAttribute("style")
-        }
-        if (email.value === "") {
-            email.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
-            good = false;
-        }
-        else {
-            email.removeAttribute("style")
-        }
-        if (pass.value.length < 6) {
-            pass.value = "";
-            pass.setAttribute("placeholder", "Password must be at least 6 chars");
-            pass.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
-            good = false;
-        }
-        if (pass_conf.value.length < 6) {
-            pass_conf.value = "";
-            pass_conf.setAttribute("placeholder", "Password must be at least 6 chars");
-            pass_conf.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+    if (pass.value === pass_conf.value && pass.value.length >= 6 && pass_conf.value.length >= 6) {
+        pass.removeAttribute("style");
+        pass_conf.removeAttribute("style");
+    }
+    else {
+        pass.value = "";
+        pass_conf.value = "";
+        pass.setAttribute("placeholder", "Passwords do not match");
+        pass_conf.setAttribute("placeholder", "Passwords do not match");
+        pass.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        pass_conf.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
+        good = false;
+    }
 
-            good = false;
-        }
-        if (pass.value === pass_conf.value && pass.value.length >= 6 && pass_conf.value.length >= 6) {
-            pass.removeAttribute("style");
-            pass_conf.removeAttribute("style");
-        }
-        else {
-            pass.value = "";
-            pass_conf.value = "";
-            pass.setAttribute("placeholder", "Passwords do not match");
-            pass_conf.setAttribute("placeholder", "Passwords do not match");
-            pass.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
-            pass_conf.setAttribute("style", "outline:2px solid red; outline-offset: -2px");
-            good = false;
-        }
+    if (good) {
+        const promise = firebase.auth().createUserWithEmailAndPassword(email.value, pass.value);
 
-        if (good) {
-            console.log('was good..');
-            const promise = firebase.auth().createUserWithEmailAndPassword(email.value, pass.value);
-
-            promise.then(function () {
-                firebase.auth().onAuthStateChanged(firebaseUser => {
-                    const database = firebase.database();
-                    database.ref("Users/" + firebaseUser.uid).set({
-                        first: first.value,
-                        last: last.value
-                    });
+        promise.then(function () {
+            firebase.auth().onAuthStateChanged(firebaseUser => {
+                firebase.database().ref("Users/" + firebaseUser.uid).set({
+                    first: first.value,
+                    last: last.value
+                }).then(function () {
+                    location.reload()
                 });
             });
-        }
-    });
-}());
+        });
+    }
+}
 
 (function () {
     firebase.auth().onAuthStateChanged(firebaseUser => {
